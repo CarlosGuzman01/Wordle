7dfd69839b2fddf810dbf8f5760776f4b5b3b417
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-01 11:36:45 -0500

    back up

--- app/src/main/java/com/example/wordleand102_project1/MainActivity.kt
+++ app/src/main/java/com/example/wordleand102_project1/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.wordleand102_project1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//        toolbar.title = "Wordle"
+//
+
+
+
+
+
+
+
+
+
+
+
     }
 }
