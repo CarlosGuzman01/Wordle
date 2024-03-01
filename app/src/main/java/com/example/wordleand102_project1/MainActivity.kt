0ac1ff0735467138785250118568df8892ccfc8f
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-01 12:12:53 -0500

    added toolbar

--- app/src/main/java/com/example/wordleand102_project1/MainActivity.kt
+++ app/src/main/java/com/example/wordleand102_project1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,10 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-//        setSupportActionBar(toolbar)
-//        toolbar.title = "Wordle"
-//
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        toolbar.title = "Wordle"
+
+
+
 
 
 
